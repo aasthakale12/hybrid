5337eeaae044272917078aa40d5cbc2cf7103dca
aasthakale12 <167960334+aasthakale12@users.noreply.github.com>
2024-11-12 11:28:41 +0530

    Update practical5.dart

--- practical5.dart
+++ practical5.dart
@@ -29,7 +29,7 @@ class _SimpleExampleAppState extends State<_SimpleExampleApp> {
 
     // Start the player as soon as the app is displayed.
     WidgetsBinding.instance.addPostFrameCallback((_) async {
-      await player.setSource(AssetSource('audio.wav'));
+      await player.setSource(AssetSource('forest-163012.mp3'));
       await player.resume();
     });
   }
